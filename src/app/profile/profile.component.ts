b2c33bf9835d9866a6e6cb243f2a724c1416c621
rouzbeh.majlessi@avanade.com <rouzbeh.majlessi@avanade.com>
2020-01-09 17:24:42 -0800

    import formbuilder formgroup, upload service and inject them via constructor

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UploadService } from './../upload.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private uploadService: UploadService,
+  ) { }
 
   ngOnInit() {
   }
