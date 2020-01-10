734b0e02b56fd1c7c49a1b3de3316cda2b51fe69
rouzbeh.majlessi@avanade.com <rouzbeh.majlessi@avanade.com>
2020-01-09 17:19:05 -0800

    add upload method to upload service which handles posting the form data to the server

--- src/app/upload.service.ts
+++ src/app/upload.service.ts
@@ -12,4 +12,23 @@ export class UploadService {
   constructor(
     private httpClient: HttpClient,
   ) { }
+
+  public upload(data, userId) {
+    let uploadURL = `${this.SERVER_URL}/auth/${userId}/avatar`;
+    return this.httpClient.post<any>(uploadURL, data, {
+      reportProgress: true,
+      observe: 'events'
+    }).pipe(map((event) => {
+      switch (event.type) {
+        case HttpEventType.UploadProgress:
+          const progress = Math.round(100 * event.loaded / event.total);
+          return { status: 'progress', message: progress};
+
+          case HttpEventType.Response:
+            return event.body;
+            default:
+              return `Unhandled event: ${event.type}`;
+      }
+    }));
+  }
 }
